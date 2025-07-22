040d2764ebba40b3038f5b8ba6f61d21f1ad7e30
Adam Saslow <adam@rarefaction.io>
2025-07-22 13:10:29 -0700

    IAnalyticsService.Screen now takes UIScreenBase as a parameter

--- UnityProject/Assets/Scripts/Util/IAnalyticsService.cs
+++ UnityProject/Assets/Scripts/Util/IAnalyticsService.cs
@@ -4,6 +4,6 @@ public interface IAnalyticsService : IService
 {
     void Register(Dictionary<string, object> parameters = null);
     void Track(string eventName, Dictionary<string, object> parameters = null);
-    void Screen(string screenName, Dictionary<string, object> parameters = null);
+    void Screen(UIScreenBase screen, Dictionary<string, object> parameters = null);
     void Flush();
 }
