1143d70b7e1f24a50dff4f6d32376bd54d93bc5b
AlexLunaArceDev <alex.luna.arce.dev@gmail.com>
2025-07-31 11:19:06 -0600

    Implemented the LocalConfigService

--- UnityProject/Assets/Scripts/Bootstrapper.cs
+++ UnityProject/Assets/Scripts/Bootstrapper.cs
@@ -2,14 +2,44 @@ using UnityEngine;
 
 public class Bootstrapper : MonoBehaviour
 {
+    public enum Cohort
+    {
+        VariantA,
+        VariantB,
+        VariantC
+    }
+    public static Cohort AssignedCohort { get; private set; }
+    private const string _cohortKey = "UserCohort";
+
     private void Awake()
     {
-        //TODO: Assign our user to a cohort of Variant A, B, or C
+        //Assign our user to a cohort of Variant A, B, or C
+        AssignCohort();
         
-        //TODO:
-        //ServiceLocator.Bind<IConfigService>(new LocalConfigService());
-        
-        //TODO:
-        //ServiceLocator.Bind<IAnalyticsService>(new LocalAnalyticsService());
+        //Binding the Local ConfigService
+        ServiceLocator.Bind<IConfigService>(new LocalConfigService());
+
+    }
+
+    private void AssignCohort()
+    {
+        if (PlayerPrefs.HasKey(_cohortKey))
+        {
+            AssignedCohort = (Cohort)System.Enum.Parse(typeof(Cohort), PlayerPrefs.GetString(_cohortKey));
+            Debug.Log($"User was aleary assigned to cohort: {AssignedCohort}");
+        }
+        else
+        {
+            AssingNewCohort();
+            Debug.Log($"Assigned new cohort: {AssignedCohort}");
+        }
+    }
+
+    [ContextMenu("AssingNewCohort")]
+    private void AssingNewCohort()
+    {
+        int randomIndex = Random.Range(0, 3);
+        AssignedCohort = (Cohort)randomIndex;
+        PlayerPrefs.SetString(_cohortKey, AssignedCohort.ToString());
     }
 }
